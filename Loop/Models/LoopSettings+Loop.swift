93c129b6c6f5ba858eb7c443aa2f02c4593dbcaf
jeremybarnum <jeremybarnum@gmail.com>
2023-04-16 23:49:17 -0400

    Set RC back to 1.0

--- Loop/Models/LoopSettings+Loop.swift
+++ Loop/Models/LoopSettings+Loop.swift
@@ -18,7 +18,7 @@ extension LoopSettings {
         return inputs
     }
 
-    static let retrospectiveCorrectionEffectDuration = TimeInterval(hours: 1.5)
+    static let retrospectiveCorrectionEffectDuration = TimeInterval(hours: 1)
 
     /*/// Creates an instance of the enabled retrospective correction implementation
     var enabledRetrospectiveCorrectionAlgorithm: RetrospectiveCorrection {
