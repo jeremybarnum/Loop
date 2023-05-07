7465cfe79b6cf263fbf851250bb4b9bf9644143f
jeremybarnum <jeremybarnum@gmail.com>
2023-05-07 17:12:33 -0400

    adding absorption ratio to notification

--- Loop/Managers/NotificationManager.swift
+++ Loop/Managers/NotificationManager.swift
@@ -229,7 +229,7 @@ extension NotificationManager {
         /// Notifications should expire after the missed meal is no longer relevant
         //let expirationDate = mealStart.addingTimeInterval(LoopCoreConstants.defaultCarbAbsorptionTimes.slow)
 
-        notification.title =  String(format: NSLocalizedString("Carbs absorbing slowly", comment: "The notification title for a slow carb absorption situation"))
+        notification.title =  String(format: NSLocalizedString("Carbs absorbing at \(absorptionRatio)% of expected", comment: "The notification title for a slow carb absorption situation"))
         notification.body = String(format: NSLocalizedString("Carbs are absorbing slowly but Loop hasn't adjusted yet. If you edit down and check zero temp effects, you can see whether you will crash.", comment: "The notification description for a slow absorbing scenario"))
         notification.sound = .default
         
