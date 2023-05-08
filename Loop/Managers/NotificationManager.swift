b4d4777060ccdc78933e44ccff8de2d31a49aa99
jeremybarnum <jeremybarnum@gmail.com>
2023-05-08 18:56:10 -0400

    fixing format of notification title

--- Loop/Managers/NotificationManager.swift
+++ Loop/Managers/NotificationManager.swift
@@ -228,8 +228,9 @@ extension NotificationManager {
         let notification = UNMutableNotificationContent()
         /// Notifications should expire after the missed meal is no longer relevant
         //let expirationDate = mealStart.addingTimeInterval(LoopCoreConstants.defaultCarbAbsorptionTimes.slow)
-        let formattedAbsorptionRatio = String(format: "%.0f%%", absorptionRatio)
-        notification.title =  String(format: NSLocalizedString("Carbs absorbing at \(formattedAbsorptionRatio) of expected", comment: "The notification title for a slow carb absorption situation"))
+        let formattedAbsorptionRatio = String(format:"%.0f%%", absorptionRatio*100)
+        //print("*test formatted absorption ratio",formattedAbsorptionRatio)
+        notification.title = String(format: NSLocalizedString("Carbs absorbing at %@ of expected", comment: "The notification title for a slow carb absorption situation"), formattedAbsorptionRatio)
         notification.body = String(format: NSLocalizedString("Carbs are absorbing slowly but Loop hasn't adjusted yet. If you edit down and check zero temp effects, you can see whether you will crash.", comment: "The notification description for a slow absorbing scenario"))
         notification.sound = .default
         
