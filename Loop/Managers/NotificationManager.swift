3a4d82a103e80ccdfcd81de80dfe14e973b14092
jeremybarnum <jeremybarnum@gmail.com>
2023-05-10 08:49:29 -0400

    clean up notification comments

--- Loop/Managers/NotificationManager.swift
+++ Loop/Managers/NotificationManager.swift
@@ -224,14 +224,12 @@ extension NotificationManager {
         UNUserNotificationCenter.current().add(request)
     }
     
-    static func sendSlowAbsorptionNotification(timeToLow: (TimeInterval,TimeInterval), delay: TimeInterval? = nil) {//TODO: this function should accept whether or not you will go low, when, and whether zero temp will fix the problem.  COnsider also quantifying rescue carbs per dragan's approach, but could be a bit pedantic.  Also - should it be three separate warnings, depending? Like Dragan did? maybe.  I shouldn't be stubborn about plagiarizing.
+    static func sendSlowAbsorptionNotification(timeToLow: (TimeInterval,TimeInterval), delay: TimeInterval? = nil) {//TODO: COnsider also quantifying rescue carbs per dragan's approach, but could be a bit pedantic.  Also - should it be three separate warnings, depending? Like Dragan did? maybe.
         let notification = UNMutableNotificationContent()
-        /// Notifications should expire after the missed meal is no longer relevant
-        //let expirationDate = mealStart.addingTimeInterval(LoopCoreConstants.defaultCarbAbsorptionTimes.slow)
-        //let formattedAbsorptionRatio = String(format:"%.0f%%", absorptionRatio*100)
-        //print("*test formatted absorption ratio",formattedAbsorptionRatio)
+
         let timeToLowInMinutes = String((timeToLow.0 / 60 ).rounded())
         let timeToLowInMinutesZeroTemp = String((timeToLow.1 / 60 ).rounded())
+        print("Time to low tuple:",timeToLow)
 
         notification.title = String(format: NSLocalizedString("Crash coming in %@ minutes", comment: "The notification title for a slow carb absorption situation"),timeToLowInMinutes)
         notification.body = String(format: NSLocalizedString("Time to low if carb entry is edited down and Loop zero temps in response: %@",comment: "The notification description for a slow absorbing scenario"),timeToLowInMinutes, timeToLowInMinutesZeroTemp)
