ed46e0730d42501502fc4a185553708f84ebd527
jeremybarnum <jeremybarnum@gmail.com>
2023-05-11 22:28:13 -0400

    rescue carb calculation change

--- Loop/Managers/NotificationManager.swift
+++ Loop/Managers/NotificationManager.swift
@@ -228,15 +228,16 @@ extension NotificationManager {
     
     static func sendSlowAbsorptionNotification(timeToLow: TimeInterval,timetoLowZeroTemp: TimeInterval, lowestBGwithZeroTemp: Double, timeToLowestBGwithZeroTemp: Double, suspendThreshold: Double) {//TODO: should it be three separate warnings, depending? Like Dragan did? maybe.
         let notification = UNMutableNotificationContent()
-
         let timeToLowInMinutes = String(Int(round(timeToLow / 60)))
         //let timeToLowInMinutesZeroTemp = String(Int(round(timetoLowZeroTemp / 60)))
         //let formattedLowestBGwithZeroTemp = String(Int(round(lowestBGwithZeroTemp)))
         let assumedRescueCarbAbsorptionTimeMinutes = 60.0
-        let absorptionFraction = max(10*60, timeToLowestBGwithZeroTemp) / assumedRescueCarbAbsorptionTimeMinutes
+        let absorptionFraction = max(10, timeToLowestBGwithZeroTemp / 60) / assumedRescueCarbAbsorptionTimeMinutes
         let assumedCSF = 10.0
         let rescueCarbs = (suspendThreshold - lowestBGwithZeroTemp) / absorptionFraction / assumedCSF
         let formattedRescueCarbs = String(Int(round(rescueCarbs)))
+        
+        print("*Test AbsorptionFraction:", absorptionFraction, "rescueCarbs:", rescueCarbs)
 
         notification.title = String(format: NSLocalizedString("Low in %@ minutes", comment: "The notification title for a slow carb absorption situation"),timeToLowInMinutes)
         notification.body = String(format: NSLocalizedString("Low in %@ minutes. %@ carbs needed.",comment: "The notification description for a slow absorbing scenario"), timeToLowInMinutes, formattedRescueCarbs)
