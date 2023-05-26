d8671c5f2770162efb8e8056a722443054c0ef9e
jeremybarnum <jeremybarnum@gmail.com>
2023-05-25 22:05:33 -0400

    documenting the settings

--- Loop/Managers/Observed Absorption/ObservedAbsorptionSettings.swift
+++ Loop/Managers/Observed Absorption/ObservedAbsorptionSettings.swift
@@ -10,21 +10,21 @@ import Foundation
 
 public struct ObservedAbsorptionSettings {
     /// don't notify if the low is coming sooner than this.  it's obvious and will annoy the user.
-    public static let dontNotifyIfSooner = TimeInterval(minutes: 5)
+    public static let dontNotifyIfSooner = TimeInterval(minutes: 10)
     
     /// don't notify if the low is coming later than this.  it's far away, things might change, and there is plenty of time for rescue carbs
-    public static let dontNotifyIfLater = TimeInterval(minutes: 45)
+    public static let dontNotifyIfLater = TimeInterval(minutes: 30)
     
     //we need to make an assumption about how fast burning the carbs are when estimating the needed rescue carbs
     public static let assumedRescueCarbAbsorptionTimeMinutes = 60.0
  
-    //when proposing rescue carbs, it's not reasonable to try to take enough to cure a low that is going to happen inside of this window
+    //when proposing rescue carbs, it only credits the amount that will get absorbed before the low hits.  But it gets exponential if the low is too soon, so this rescue carb multiplier effect needs to be limited
     public static let flooredTimeForRescueCarbs = 10.0
     
     //to avoid repeating warnings, don't warn if the warning has happened more recently than this
     public static let notificationInterval = TimeInterval(minutes: 6)
     
-    //when calculating the observedAbsorption ratio, use a recent observation window that is this
+    //when calculating the observedAbsorption ratio, use a recent observation window that is this number
     public static let observationWindow = TimeInterval(minutes: 30)
     
     //when adjusting future carb absorption, don't adjust very recent carb entries or entries in the future, since these may well be rescue carbs.  Some overlap between this setting and the warning delay
