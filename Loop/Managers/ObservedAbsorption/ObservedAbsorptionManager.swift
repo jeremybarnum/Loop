d1a32f5842e4c0789f6419c461bc686c3ffd6913
Jeremy Barnum <jeremybarnum@gmail.com>
2023-10-23 12:15:04 -0400

    add absorption files and commit the weird watch thing

---
+++ Loop/Managers/ObservedAbsorption/ObservedAbsorptionManager.swift
@@ -0,0 +1,9 @@
+//
+//  ObservedAbsorptionManager.swift
+//  Loop
+//
+//  Created by Jeremy Barnum on 10/23/23.
+//  Copyright © 2023 LoopKit Authors. All rights reserved.
+//
+
+import Foundation
---
+++ Loop/Managers/ObservedAbsorption/ObservedAbsorptionSettings.swift
@@ -0,0 +1,9 @@
+//
+//  ObservedAbsorptionSettings.swift
+//  Loop
+//
+//  Created by Jeremy Barnum on 10/23/23.
+//  Copyright © 2023 LoopKit Authors. All rights reserved.
+//
+
+import Foundation
