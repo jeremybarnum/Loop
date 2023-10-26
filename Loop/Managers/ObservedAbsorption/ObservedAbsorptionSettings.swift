c26f57c5e1d4a5aa399d03c1332a6a6d1ddf24c7
Jeremy Barnum <jeremybarnum@gmail.com>
2023-10-25 23:32:36 -0400

    Squashed commit of the following:
    commit c357ef965e2c601f20acd3a651aefcc99f0d1af0
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Tue Oct 24 15:32:50 2023 -0400
    
        Add observedAbsorptionEffect to prediction screen
    
    commit 7bae91f7dc732b7f447d4e29b88a2fe80d9f8064
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 20:34:29 2023 -0400
    
        print statements for testing
    
    commit 9e219b908938afb79c2786cc25c670255a776243
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 20:33:15 2023 -0400
    
        temporary changes to settings for testing
    
    commit ead536388b49c463a01e5282e852e830c08b9c9c
    Merge: bc61c55f 7c60cacb
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 20:29:51 2023 -0400
    
        Merge remote-tracking branch 'refs/remotes/origin/dev'
    
    commit bc61c55fe39c10c04a819c9c988abc650c964a34
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 20:22:58 2023 -0400
    
        fix merge problem with updating AlertManagementView
    
    commit b649a039cc829a0ed339173c90f56b1264937db9
    Author: jeremybarnum <jeremybarnum@gmail.com>
    Date:   Sat May 13 00:06:18 2023 -0400
    
        add slow absorption toggle to the view
    
        (cherry picked from commit e8cb4c51981c28a6cff8a21c8ed4dc455424dcf7)
    
    commit e699bec24e2f3b0bfad1cfa68172ab93a4d6f1bb
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 19:51:21 2023 -0400
    
        replace the wrong notification category due to missing enum with a string describing the category accurately
    
    commit b3799fca2c84740bcf926b075075f8fa54f628f5
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 19:47:33 2023 -0400
    
        fix errors in NotificationManager by using a placeholder for the notification category to avoid having to maintain a separate branch of loopkit
    
    commit 37666e22efc72772ed8bacd748d0d56bfbfd961f
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 19:40:02 2023 -0400
    
        copy paste notification code into NotificationManger
    
    commit ef977afde73b375d8e127688444ed3121df9aa86
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 19:32:10 2023 -0400
    
        replace .zeroTemp with .suspend in LDM
    
    commit b47697d3e933f683b275a04cf9ef0542a56f216d
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 19:29:47 2023 -0400
    
        updating prediction input effect to include slow absorption
    
    commit 6b0a421bfd98f32b359d1a7395fb8438f7d95d4f
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 19:24:18 2023 -0400
    
        finishLDM changes.  Now other files need to be changed
    
    commit e78d251721214213aeb26153899cd4c8fb0908e7
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 18:32:08 2023 -0400
    
        Debugging LDM copy paste
    
    commit b6e0a6691d3aa2513a40ca1902fd9b199bbb5522
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 18:21:27 2023 -0400
    
        first attempt at copying in all necessary code for alterts into LoopDataManager
    
    commit d8114521e39aa2fce05a66d449c59f0f743eae04
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 12:20:08 2023 -0400
    
        Paste in ObservedAbsorptionSettings
    
    commit 1f851581119b50b568c728c64a12cff2162552e5
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 12:18:59 2023 -0400
    
        Paste in ObservedAbsorptionManager Code
    
    commit 58319079b7091638f680973cd289d65aa9e8d3d8
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 12:16:43 2023 -0400
    
        Project and watch app
    
    commit d1a32f5842e4c0789f6419c461bc686c3ffd6913
    Author: Jeremy Barnum <jeremybarnum@gmail.com>
    Date:   Mon Oct 23 12:15:04 2023 -0400
    
        add absorption files and commit the weird watch thing

---
+++ Loop/Managers/ObservedAbsorption/ObservedAbsorptionSettings.swift
@@ -0,0 +1,52 @@
+//
+//  ObservedAbsorptionSettings.swift
+//  Loop
+//
+//  Created by Jeremy Barnum on 5/12/23.
+//  Copyright © 2023 LoopKit Authors. All rights reserved.
+//
+
+import Foundation
+
+public struct ObservedAbsorptionSettings {
+    /// don't notify if the low is coming sooner than this.  it's obvious and will annoy the user.
+    public static let dontNotifyIfSooner = TimeInterval(minutes: 1)
+    
+    /// don't notify if the low is coming later than this.  it's far away, things might change, and there is plenty of time for rescue carbs
+    public static let dontNotifyIfLater = TimeInterval(minutes: 90)
+    
+    //we need to make an assumption about how fast burning the carbs are when estimating the needed rescue carbs
+    public static let assumedRescueCarbAbsorptionTimeMinutes = 60.0
+    
+    //when proposing rescue carbs, it only credits the amount that will get absorbed before the low hits.  But it gets exponential if the low is too soon, so this rescue carb multiplier effect needs to be limited
+    public static let flooredTimeForRescueCarbs = 30.0
+    
+    //to avoid repeating warnings, don't warn if the warning has happened more recently than this
+    public static let notificationInterval = TimeInterval(minutes: 1)
+    
+    //when calculating the observedAbsorption ratio, use a recent observation window that is this number
+    public static let observationWindow = TimeInterval(minutes: 30)
+    
+    //when adjusting future carb absorption, don't adjust very recent carb entries or entries in the future, since these may well be rescue carbs.  Some overlap between this setting and the warning delay
+    public static let recentAndFutureCarbExclusionWindow = TimeInterval(minutes: 15)
+    
+    //don't warn if the carbs have been taken inside on this window, to avoid warnings in low confidence situations
+    public static let warningDelay = TimeInterval(minutes: 35)
+    
+    //don't produce observed absorption efects until there are at least 3 observed carb effects
+    public static let minCarbEffectCount = 3.0
+    
+    //toggle betweeen insulin-centric notification and carb centric notification.  If COB is Nil or very low, the assumption is it's all insulin driven, and if zero temping isn't enough, rescue carbs are defiitely needed.
+    public static let minCOBforRescueCarbsDefinitelyNeeed = 3.0
+    
+    //user can decide how much OAE they want to add to IRC.  0% is like turning off warning, 100% is probably a lot of double count
+    public static let IRC_OCA_blend = 0.65
+    
+    //user can decide how much OAE they want to add to IRC.  Below 25% is like turning off the warnings.
+    public static let lowEndOfIRC_OCASlider = 0.25
+    
+    //user can decide how much OAE they want to add to IRC.  Above 75% is probably a lot of double counting and will lead to overwarning.
+    public static let highEndOfIRC_OCASlider = 0.75
+    
+}
+
