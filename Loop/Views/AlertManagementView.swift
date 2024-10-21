de811b9312b69c611c0c641c393854855e9ab00f
Jeremy Barnum <jeremybarnum@gmail.com>
2024-10-20 22:21:49 -0400

    final tweaks to UI

--- Loop/Views/AlertManagementView.swift
+++ Loop/Views/AlertManagementView.swift
@@ -297,7 +297,7 @@ struct AlertManagementView: View {
                         }
                     }
                     .pickerStyle(WheelPickerStyle())
-                    .frame(height: 70) // Set the desired height
+                    .frame(height: 80) // Set the desired height
                 }
             }
         }
